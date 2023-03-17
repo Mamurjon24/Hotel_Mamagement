e16d9249348e52b06684fc1829e36a99bb453289
Mamurjon <uzokov29@gmail.com>
2023-03-17 19:27:23 +0500

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //https://github.com/Mamurjon24/Hotel_Mamagement.git
+        //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
+        System.out.println("Hello world!");
+    }
+}
